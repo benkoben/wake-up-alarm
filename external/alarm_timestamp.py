97edb7128323a5ac03ddcd0d2db63fd3b89ea806
Ben Kooijman <ben@kooijman.se>
2026-01-19 20:45:08 +0100

    timezones are now working

--- external/alarm_timestamp.py
+++ external/alarm_timestamp.py
@@ -1,106 +1,174 @@
-from datetime import datetime, timedelta
+import usocket as socket
+import struct
+import time
 
+from datetime import date
 
-class AlarmTimestamp():
-    def __init__(self, hour=None, minute=None):
-        now = datetime.now()
-        self.timestamp = now
-        if hour:
-            self.timestamp.replace(hour=hour)
-        if minute:
-            self.timestamp.replace(minute=minute)
+from config import NetworkConfig
 
-    def increase_minute(self):
-        self.timestamp += timedelta(minutes=1)
-        self.adjust_for_future()
-        print(f"today is: {datetime.now()}")
-        print(f"timestamp is now set to: {self.timestamp}")
+_tz = {
+	"CEST": +2, # Summetime
+	"CET": +1
+}
 
-    def decrease_minute(self):
-        self.timestamp -= timedelta(minutes=1)
-        self.adjust_for_future()
-        print(f"today is: {datetime.now()}")
-        print(f"timestamp is now set to: {self.timestamp}")
+"""
+Ntp fetches the current time from host upon initialization.
+"""
+class Ntp():
+	def __init__(self, host):
+		
+		# Fetch UTC time from NTP server
+		self.current_time = time.gmtime(
+			self.fetch_current_time(host)
+        )
+		
+	def year(self):
+        #     year includes the century (for example 2014).
+		return self.current_time[0]
+        
+	def month(self):
+        #     month is 1-12
+		return self.current_time[1]
+	
+	def monthday(self):
+        #     monthday is 1-31
+		return self.current_time[2]
+	
+	def hour(self):
+        #     hour is 0-23
+		return self.current_time[2]
 
-    # adjusts the timestamp to one day in the future if
-    # the current timestamp is less than datetime.now()
-    def adjust_for_future(self):
-        now = datetime.now()
+	def minute(self):
+        #     minute is 0-59
+		return self.current_time[4]
+	
+	def second(self):
+        #     second is 0-59
+		return self.current_time[5]
+	
+	def weekday(self):
+        #     weekday is 0-6 for Mon-Sun
+		return self.current_time[6]
+	
+	def yearday(self):
+        #     yearday is 1-366
+		return self.current_time[7]
+	
+	def timestamp(self):
+		return f"{self.year()}/{self.month()}/{self.monthday()} {self.hour()}:{self.minute()}:{self.second()} weekday: {self.weekday()}"
 
-        ts = self.timestamp
+	def _is_sunday(self, year, month, day) -> bool:
+		t = (year, month, day, 0, 0, 0, 0, 0)
+		return time.localtime(time.mktime(t))[6] == 6
 
-        # Compare only hours and minutes
-        ts_hhmmss = ts.hour * 60 + ts.minute * 60 + ts.second
-        now_hhmmss = now.hour * 60 + now.minute * 60 + now.second
+	"""
+		_sunday returns a sunday as a epoch timestamp
+	"""	
+	def _sunday(self, year, month, day) -> int:
+		yearday = date(year, month, day).timetuple()[-2]
+		return time.mktime((
+			year,
+			month,
+			day,
+			0, # hour
+			0, # minute
+			0, # seconds
+			6, # weekday
+			yearday, # TODO: I need to figure out the yearday of this particular sunday
+        ))
 
-        # Determine if timestamp is in the past, today, or future (by day)
-        if ts.date() > now.date():
-            if ts_hhmmss < now_hhmmss:
-                # Future day, earlier time → do nothing
-                return
-            else:
-                # Future day, later time → set to today
-                self.timestamp = self.timestamp.replace(
-                    year=now.year, month=now.month, day=now.day
-                )
-                return
+	"""
+		_last_sunday_in_march returns the current year's last sunday in March as a Datetime object
+	"""
+	def _last_sunday_in_march(self) -> int:
+		# Loop trough the last week in march and find sunday
+		year = self.year()
+		for day in range(31, 24, -1):
+			if self._is_sunday(year, 3, day):
+				return int(self._sunday(
+					year, 3, day
+				))
+		return 0
 
-        elif ts.date() < now.date():
-            if ts_hhmmss < now_hhmmss:
-                # Past day, earlier time → set to tomorrow
-                new_date = now + timedelta(days=1)
-                self.timestamp = self.timestamp.replace(
-                    year=new_date.year, month=new_date.month, day=new_date.day
-                )
-                return
-            else:
-                # Past day, later time → set to today
-                self.timestamp = self.timestamp.replace(
-                    year=now.year, month=now.month, day=now.day
-                )
-                return
+	"""
+		_last_sunday_in_march returns the current year's last sunday in October as a Datetime object
+	"""
+	def _last_sunday_in_october(self) -> int:
+		# Loop trough the last week in october and find sunday
+		year = self.year()
+		for day in range(31, 24, -1):
+			if self._is_sunday(year, 10, day):
+				return int(self._sunday(
+					year, 10, day
+				))
+		return 0
 
-        # If it's today, and still in the future — leave it alone
-        if ts_hhmmss < now_hhmmss:
-            # Still today, but earlier than now → bump to tomorrow
-            new_date = now + timedelta(days=1)
-            self.timestamp = self.timestamp.replace(
-                year=new_date.year, month=new_date.month, day=new_date.day
-            )
+	def adjust_for_daylight_savings(self):
+		new_ts = time.mktime(self.current_time)
+		current_epoch = time.mktime(self.current_time)
+		if current_epoch > self._last_sunday_in_march() and current_epoch < self._last_sunday_in_october():
+			# Adjust the hour
+			new_ts += _tz["CEST"] * 3600
+		else:	
+			new_ts += _tz["CET"] * 3600
+		self.current_time = time.gmtime(new_ts)
+			
+		
+	def fetch_current_time(self, NTP_HOST: str) -> int:
+		# Used to convert the received transmit_timestamp to unix time.
+		NTP_DELTA = 2208988800
+		# Create a 48 byte packet
+		NTP_QUERY = bytearray(48)
+		# First byte (LI=0, VN=3, Mode=3 for client mode)
+		NTP_QUERY[0] = 0x1B
+		
+		UNIX_TIME=""
+		# Set the target address and port
+		addr = socket.getaddrinfo(NTP_HOST, 123)[0][-1]
+		# Create a datagram UDP socket
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		try:
+			s.settimeout(1)
+			s.sendto(NTP_QUERY, addr)
+			msg = s.recv(48)
 
-    def get_current(self):
-        return self.__str__()
+			print(f"Response recieved: {msg}")
+			# The response is also a 48 byte packet where the last 8 bytes contain the "transmit timestamp"
+			transmit_timestamp = struct.unpack("!I", msg[40:44])[0]
 
-    def get_current_with_refresh(self):
-        self.refresh_current()
-        return self.get_current()
+			# TODO: what does the transmit timestmap mean?
+			print(f"Transmit timestamp: {transmit_timestamp}")
+			UNIX_TIME = transmit_timestamp - NTP_DELTA
 
-    def reset_seconds(self):
-        self.timestamp = self.timestamp.replace(second=0)
-        #print(self.__repr__())
+		except socket.timeout:
+			print("Timeout: No response from NTP server")
+		except Exception as e:
+			print(f"Error occurred: {e}")
+		finally:
+			s.close()
 
-    # def __repr__(self):
-    #     return f"{self.__class__.__name__}: {self.timestamp.date()}"
+		return UNIX_TIME
+	
 
-    def __str__(self) -> str:
-        hour = str(self.timestamp.hour)
-        minute = str(self.timestamp.minute)
-        # add paddings
-        hour = "0" * max(0, 2 - len(hour)) + hour
-        minute = "0" * max(0, 2 - len(minute)) + minute
+if __name__ == "__main__":
+	now = Ntp(host="pool.ntp.org")
 
-        ts = "{}{}".format(hour, minute)
-        print(ts)
-        return ts
+	# In UTC format
+	print("timestamp recieved from NTP")
+	print(now.current_time)
 
-    def __eq__(self, other):
-        return other.timestamp == self.timestamp
+	# Adjust for current timezone
+	print("timestamp after timezone adjustment (winter time)")
+	# Expect 1 hour shift since its currenly january
+	now.adjust_for_daylight_savings()
+	print(now.current_time)
 
-    def __gt__(self, other):
-        return self.timestamp > other.timestamp
+	# Adjust the date to test summertime as well
+	print("adjust timestamp to timestamp (summertime)")
+	now.current_time = time.gmtime(1753213285)
+	print("before summertime adjustment")
+	print(now.current_time)
 
-    def __lt__(self, other):
-        return self.timestamp < other.timestamp
-
-    def refresh_current(self):
-        self.timestamp = datetime.now()
+	now.adjust_for_daylight_savings()
+	print("after summertime adjustment")
+	print(now.current_time)
